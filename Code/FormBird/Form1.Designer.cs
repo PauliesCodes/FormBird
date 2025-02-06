0e9ea58a5092180663180c3d5715f1f013b719d5
Michal Paulas <michal123paulas@gmail.com>
2025-02-06 11:26:32 +0100

    Base Game Added

--- Code/FormBird/Form1.Designer.cs
+++ Code/FormBird/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             this.title_Label = new System.Windows.Forms.Label();
             this.play_Button = new System.Windows.Forms.Button();
@@ -37,6 +38,8 @@
             this.gitHub_PictureBox = new System.Windows.Forms.PictureBox();
             this.pipe_color_PictureBox = new System.Windows.Forms.PictureBox();
             this.bird_color_PictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clear_Button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.url_PictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gitHub_PictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipe_color_PictureBox)).BeginInit();
@@ -59,9 +62,9 @@
             this.play_Button.BackColor = System.Drawing.SystemColors.Window;
             this.play_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.play_Button.Font = new System.Drawing.Font("Yu Gothic", 24F, System.Drawing.FontStyle.Bold);
-            this.play_Button.Location = new System.Drawing.Point(104, 108);
+            this.play_Button.Location = new System.Drawing.Point(35, 108);
             this.play_Button.Name = "play_Button";
-            this.play_Button.Size = new System.Drawing.Size(171, 58);
+            this.play_Button.Size = new System.Drawing.Size(146, 58);
             this.play_Button.TabIndex = 1;
             this.play_Button.Text = "PLAY";
             this.play_Button.UseVisualStyleBackColor = false;
@@ -137,12 +140,31 @@
             this.bird_color_PictureBox.TabStop = false;
             this.bird_color_PictureBox.Click += new System.EventHandler(this.bird_color_PictureBox_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clear_Button
+            // 
+            this.clear_Button.BackColor = System.Drawing.SystemColors.Window;
+            this.clear_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clear_Button.Font = new System.Drawing.Font("Yu Gothic", 24F, System.Drawing.FontStyle.Bold);
+            this.clear_Button.Location = new System.Drawing.Point(194, 108);
+            this.clear_Button.Name = "clear_Button";
+            this.clear_Button.Size = new System.Drawing.Size(146, 58);
+            this.clear_Button.TabIndex = 8;
+            this.clear_Button.Text = "CLEAR";
+            this.clear_Button.UseVisualStyleBackColor = false;
+            this.clear_Button.Click += new System.EventHandler(this.clear_Button_Click);
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(381, 290);
+            this.Controls.Add(this.clear_Button);
             this.Controls.Add(this.url_PictureBox);
             this.Controls.Add(this.gitHub_PictureBox);
             this.Controls.Add(this.pipe_color_PictureBox);
@@ -175,6 +197,8 @@
         private System.Windows.Forms.Label pipe_color_Label;
         private System.Windows.Forms.PictureBox gitHub_PictureBox;
         private System.Windows.Forms.PictureBox url_PictureBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button clear_Button;
     }
 }
 
--- Code/FormBird/Form1.cs
+++ Code/FormBird/Form1.cs
@@ -18,8 +18,24 @@ namespace FormBird
             InitializeComponent();
         }
 
-        Color bird = Color.Yellow;
+        Color birdColor = Color.Yellow;
         Color pipe = Color.Green;
+
+        public List<Form> pipes = new List<Form>();
+
+        Random random = new Random();
+        Form bird;
+        Form score;
+        Label label;
+
+        int scoreNum = 0;
+
+        int jumpedFrom = 0;
+        bool jumping = false;
+        int jumpHeight = 150;
+
+        int gravityAdder = 0;
+        int jumpAdder = 0;
 
         private void bird_color_PictureBox_Click(object sender, EventArgs e)
         {
@@ -27,8 +43,8 @@ namespace FormBird
             {
                 if (colorDialog.ShowDialog() == DialogResult.OK)
                 {
-                    bird = colorDialog.Color; 
-                    bird_color_PictureBox.BackColor = bird;
+                    birdColor = colorDialog.Color; 
+                    bird_color_PictureBox.BackColor = birdColor;
                 }
             }
         }
@@ -45,9 +61,231 @@ namespace FormBird
             }
         }
 
-        private void play_Button_Click(object sender, EventArgs e)
+        private void terminateGame()
+        {
+            try
+            {
+                foreach (Form f in pipes.ToList())
+                {
+                    if (f != null)
+                    {
+                        f.Close();
+                    }
+                }
+
+                pipes.Clear();
+
+                if (bird != null)
+                {
+                    bird.Close();
+                    bird = null;
+                }
+
+                if (score != null)
+                {
+                    score.Close();
+                    score = null;
+                }
+
+                timer1.Enabled = false;
+                jumping = false;
+                jumpAdder = 0;
+                gravityAdder = 0;
+
+                this.Focus();
+                this.WindowState = FormWindowState.Normal;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při ukončení hry: " + ex.Message);
+            }
+        }
+
+
+        private void showScoreMerer()
+        {
+            score = new Form();
+            score.Size = new Size(300, 150);
+            score.StartPosition = FormStartPosition.Manual;
+            score.Location = new Point(1920 - 300, 0);
+            score.FormBorderStyle = FormBorderStyle.None; 
+            score.ControlBox = false;                      
+            score.ShowInTaskbar = false;                   
+            score.BackColor = Color.FromArgb(64, 64, 64);
+
+            label = new Label();
+            label.Size = new Size(300, 120);
+            label.Text = "Score: 0";
+            label.Font = new Font("Yu Gothic", 48, FontStyle.Bold); 
+            label.ForeColor = Color.White; 
+            label.TextAlign = ContentAlignment.MiddleCenter; 
+            label.Dock = DockStyle.Fill;
+
+
+            score.Controls.Add(label);
+            score.Show();
+
+            score.TopMost = true;
+
+        }
+
+
+        private void spawnBird()
+        {
+            bird = new Form();
+            bird.Size = new Size(100, 100);
+            bird.StartPosition = FormStartPosition.Manual;
+            bird.Location = new Point(200, 540);
+            bird.BackColor = birdColor;
+            bird.FormBorderStyle = FormBorderStyle.None;  
+            bird.ControlBox = false;                      
+            bird.TopMost = true;                         
+            bird.ShowInTaskbar = false;                  
+            bird.BackColor = birdColor;                   
+            bird.KeyDown += Bird_KeyDown;
+            bird.KeyPress += Bird_Click;
+            bird.Show();
+        }
+
+
+        private void Bird_Click(object sender, EventArgs e)
         {
 
+            timer1.Enabled = true;
+
+        }
+
+        private void Bird_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravityAdder = 0;
+                jumpedFrom = bird.Location.Y;
+                jumping = true;
+
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+
+                terminateGame();
+
+            }
+
+        }
+
+        private void moveBird()
+        {
+            if (jumping)
+            {
+
+                bird.Location = new Point(bird.Location.X, bird.Location.Y - jumpAdder * 2);
+                jumpAdder++;
+
+
+
+
+                if (bird.Location.Y < jumpedFrom - jumpHeight)
+                {
+
+                    jumping = false;
+                    jumpAdder = 0;
+
+                }
+
+            }
+            else
+            {
+
+                bird.Location = new Point(bird.Location.X, bird.Location.Y + gravityAdder);
+                gravityAdder++;
+
+            }
+
+            if (bird.Location.Y > 1080) terminateGame();
+
+
+        }
+
+        private void createPipe(int vyska, int misto)
+        {
+            Form pipeDown = new Form();
+            pipeDown.Size = new Size(150, vyska);
+            pipeDown.StartPosition = FormStartPosition.Manual;
+            pipeDown.Location = new Point(misto, 1080 - vyska);
+            pipeDown.BackColor = pipe;
+            pipeDown.FormBorderStyle = FormBorderStyle.None;
+            pipeDown.ControlBox = false;                     
+            pipeDown.ShowInTaskbar = false;                  
+            pipeDown.TopMost = true;                         
+            pipes.Add(pipeDown);
+            pipeDown.Show();
+
+            Form pipeTop = new Form();
+            pipeTop.Size = new Size(150, 1080 - vyska - 400);
+            pipeTop.StartPosition = FormStartPosition.Manual;
+            pipeTop.Location = new Point(misto, 0);
+            pipeTop.BackColor = pipe;
+            pipeTop.FormBorderStyle = FormBorderStyle.None;  
+            pipeTop.ControlBox = false;                      
+            pipeTop.ShowInTaskbar = false;                   
+            pipeTop.TopMost = true;                          
+            pipes.Add(pipeTop);
+            pipeTop.Show();
+        }
+
+
+        private void movePipe(Form pipe, int jakMoc)
+        {
+
+            pipe.Location = new Point(pipe.Location.X - jakMoc, pipe.Location.Y);
+
+
+        }
+
+        private void checkIfBirdHitPipe()
+        {
+
+            for (int i = 0; i < pipes.Count; i++)
+            {
+
+                if (bird.Bounds.IntersectsWith(pipes[i].Bounds))
+                {
+
+                    terminateGame();
+
+                }
+
+            }
+        }
+
+        private void isPipeInEnd(Form pipe, List<Form> pipes)
+        {
+
+            if (pipe.Location.X == 0)
+            {
+                pipes.Remove(pipe);
+                pipe.Close();
+                scoreNum++;
+
+                label.Text = (scoreNum / 2).ToString();
+            }
+
+        }
+
+        private void play_Button_Click(object sender, EventArgs e)
+        {
+            gravityAdder = 0;
+            scoreNum = 0;
+
+            this.WindowState = FormWindowState.Minimized;
+            createPipe(random.Next(1, 4) * 100, 900);
+            createPipe(random.Next(1, 4) * 100, 1300);
+            createPipe(random.Next(1, 4) * 100, 1700);
+            createPipe(random.Next(1, 4) * 100, 2100);
+            showScoreMerer();
+            spawnBird();
         }
 
         private void gitHub_PictureBox_Click(object sender, EventArgs e)
@@ -66,6 +304,35 @@ namespace FormBird
                 FileName = "https://paulas-michal.mzf.cz/",
                 UseShellExecute = true 
             });
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bird.Focus();
+
+            for (int i = 0; i < pipes.Count; i++)
+            {
+
+                movePipe(pipes[i], 5);
+                isPipeInEnd(pipes[i], pipes);
+
+            }
+
+            if (pipes.Count < 8)
+            {
+
+                createPipe(random.Next(1, 4) * 100, 1920 - 150);
+                bird.Focus();
+
+            }
+
+            moveBird();
+            checkIfBirdHitPipe();
+        }
+
+        private void clear_Button_Click(object sender, EventArgs e)
+        {
+            terminateGame();
         }
     }
 }
